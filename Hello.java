14c59872ea74abb1ad95c433a41cefbbd27e06cb
Abdul Azim Shohan <abdulazimshohan03@gmail.com>
2025-08-19 22:59:28 +0600

    Data Type Learning

--- Hello.java
+++ Hello.java
@@ -2,6 +2,8 @@ public class Hello {
 
     public static void main(String args []) {
 
+        /*
+
         int num1 = 5;
         int num2 = 9;
         double num3 = 10;
@@ -9,7 +11,17 @@ public class Hello {
         System.out.println(num1 + num2);
         System.out.println(num2 - num1);
         System.out.println(result);
-        System.out.println(result / num3);
+        System.out.println(result / num3); 
+
+        */
+
+        // byte b = 125;
+        int a = 12;
+        byte k = (byte) a;
+
+        System.out.println(k);
+
+
         
 
     }
